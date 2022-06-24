afb37735d8a32a84c185c7baa8aeb5589c9ad1e4
GalBM <92295098+GalBM@users.noreply.github.com>
2022-06-24 14:06:09 +0300

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,15 +1,55 @@
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories,left_on='id',right_on='id')
+    return df
+
 
 
 def clean_data(df):
-    pass
+    # create a dataframe of the 36 individual category columns
+    categories = df["categories"].str.split(";",expand=True)
+    # select the first row of the categories dataframe
+    row = categories.loc[0]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x: x[:-2])
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1])
+        # convert column from string to numeric
+        categories[column] = categories[column].apply(lambda x: int(x))
+    # drop the original categories column from `df`
+    df.drop(axis=1,columns=['categories'],inplace=True)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df=pd.concat([df,categories], axis=1)
+    
+    # drop duplicates
+    df=df.drop_duplicates()
+    
+    # drop 2
+    # Remove rows with a  value of 2 from df
+    for i in df.columns:
+        df = df[df[i] != 2]
+    return  df
+
+
 
 
 def save_data(df, database_filename):
+    engine = create_engine('sqlite:///ETL_Preparation.db')
+    df.to_sql('data_disaster', engine, index=False, if_exists='replace')
     pass  
 
 

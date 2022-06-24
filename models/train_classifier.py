1c75cbcb5248942a5c409c587b7b9c1d2252e7f7
GalBM <92295098+GalBM@users.noreply.github.com>
2022-06-24 14:05:30 +0300

    Update train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,32 +1,86 @@
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+from sqlalchemy import create_engine
+
+import re
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+import numpy as np
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score
+from sklearn.metrics import recall_score
+from sklearn.metrics  import precision_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report
+import pickle
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///ETL_Preparation.db')
+    df = pd.read_sql_table('data_disaster', engine)
+    X = df["message"]
+    Y = df.iloc[:, 4:]
+    category_names = Y.columns
+    return X, Y, category_names 
 
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens 
 
 
 def build_model():
-    pass
+    pipeline_random_forest = Pipeline([
+    ('vect',CountVectorizer(tokenizer=tokenize)),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultiOutputClassifier(RandomForestClassifier()))
+])
+    parameters = {
+        'clf__estimator__n_estimators': [4],
+        'clf__estimator__min_samples_split': [3],
+    
+    }
+    model = GridSearchCV(pipeline_random_forest, param_grid=parameters, n_jobs=4, verbose=2, cv=3)    
+    return model
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
-
+def evaluate_model(model, X_test, y_test, category_names):
+    y_pred = model.predict(X_test)
+    class_report = classification_report(y_test, y_pred, target_names=category_names)
+    print(class_report)
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X, y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
         
         print('Building model...')
         model = build_model()
